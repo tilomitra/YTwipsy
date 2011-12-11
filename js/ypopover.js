f3d13d63272c00f608d5291e028767234c8966ad
Tilo Mitra <tilomitra@gmail.com>
2011-12-11 05:03:43 -0500

    add variable names for cleanup, add popover templates, add _createTooltipNode() overridden method

--- js/ypopover.js
+++ js/ypopover.js
@@ -1,5 +1,37 @@
 YUI.add('popover', function(Y) {
 
+	var getCN = Y.ClassNameManager.getClassName,
+	
+	DATA_TITLE = "data-title",
+	
+	TWIPSY = 'twipsy',
+	POPOVER = 'popover',
+	WRAPPER = 'wrapper',
+	HEADER = 'header',
+	INNER = 'inner',
+	TIP = 'tip',
+	FADE = 'fade',
+	IN = 'in',
+	UPARROW = "arrow-up",
+	DOWNARROW = "arrow-down",
+	LEFTARROW = "arrow-left",
+	RIGHTARROW = "arrow-right",
+	CLASSES = {
+		tooltip: getCN(POPOVER, WRAPPER),
+		header: getCN(POPOVER, HEADER),
+		inner: getCN(POPOVER, INNER),
+		tip: getCN(POPOVER, TIP),
+		fade: getCN(POPOVER, FADE),
+		fadeIn: getCN(POPOVER, IN),
+		upArrow: getCN(POPOVER, UPARROW),
+		downArrow: getCN(POPOVER, DOWNARROW),
+		leftArrow: getCN(POPOVER, LEFTARROW),
+		rightArrow: getCN(POPOVER, RIGHTARROW)
+	},
+	TEMPLATES = {
+		wrapper: '<div class="' + CLASSES.tooltip + '"><div class="' + CLASSES.header + '">{title}</div><div class="' + CLASSES.inner + '">{content}</div></div>',
+		tip: '<div class="' + CLASSES.tip + '"></div>'
+	};
 	function Popover(config) {
 	    Popover.superclass.constructor.apply(this, arguments);
 	}
@@ -20,16 +52,41 @@ YUI.add('popover', function(Y) {
 	 * The default set of attributes for the AnimPlugin class.
 	 */
 	Popover.ATTRS = {
-
+		title: {
+			value: "",
+			validator: Y.Lang.isString
+		},
+		template : {
+			value: TEMPLATES.wrapper,
+			validator: Y.Lang.isString
+		},
+		_tip: {
+			value: TEMPLATES.tip,
+			validator: Y.Lang.isString
+		}
 	};
 
 	Y.extend(Popover, Y.Twipsy, {
 
 	    // Lifecycle methods
 	     _handle : undefined,
-
+		_host: undefined,
+		
 		initializer : function(cfg) {
-
+			this._host = this.get("host");
+			if (this._host.getAttribute("rel") == TWIPSY) {
+				this.set("title", this._host.getAttribute(DATA_TITLE));
+			}
+		},
+		
+		_createTooltipNode : function () {
+			var node = Y.Node.create(
+				Y.Lang.sub(this.get("template"), {
+					content: this.get("content"),
+					title: this.get("title")
+				})
+			);
+			return node;
 		},
 
 		//clean up on destruction
